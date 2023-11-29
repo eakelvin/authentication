3b551bb3a65a03d139d64b75fbfeac6ed84e7a08
Kelvin Akaba <akabakelvin@gmail.com>
2023-11-29 13:42:02 -0800

    server commit

---
+++ backend/src/routes/userRoute.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+const { 
+        authUser, 
+        registerUser, 
+        logoutUser, 
+        getUserProfile, 
+        updateUserProfile 
+} = require('../controllers/userController')
+const { protect } = require('../middleware/authMiddleware')
+
+router.post('/register', registerUser)
+router.post('/auth', authUser)
+router.post('/logout', logoutUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+
+module.exports = router
