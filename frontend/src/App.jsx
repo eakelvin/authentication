a9a800af493798a47c17ab17ea22ded3571b9e4d
Kelvin Akaba <akabakelvin@gmail.com>
2024-03-07 16:41:27 +0000

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
+import Dashboard from './Pages/Dashboard'
 import Home from './Pages/Home'
-import Login from './Pages/login'
-import Signup from './Pages/signup'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
 import Profile from './Pages/Profile'
+import PrivateRoute from './Components/PrivateRoute'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
-import PrivateRoute from './Components/PrivateRoute'
-import Dashboard from './Pages/Dashboard'
 
 function App() {
   return (
