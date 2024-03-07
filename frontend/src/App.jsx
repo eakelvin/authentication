9cd0848fd48f7b6befc6c475a923161a76e82bcd
Kelvin Akaba <akabakelvin@gmail.com>
2024-03-07 16:27:15 +0000

    route fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
-import Home from './Pages/home'
+import Home from './Pages/Home'
 import Login from './Pages/login'
 import Signup from './Pages/signup'
 import Profile from './Pages/Profile'
